880afa5a3df349b241b36f94dbeeacf31182ae88
Collin Herber <rustaddicts@gmail.com>
2021-10-12 19:02:57 -0400

    feat: create navbar and do major cleanup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Aurelia, { RouterConfiguration } from 'aurelia';
 import { App } from './app';
 
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
 Aurelia
     .register(RouterConfiguration)
     .app(App)
