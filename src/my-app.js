32bbf71963df4105d8de0cd5d637f757a7011387
Collin Herber <rustaddicts@gmail.com>
2021-10-12 18:30:59 -0400

    initial

---
+++ src/my-app.js
@@ -0,0 +1,13 @@
+import { route } from '@aurelia/router';
+
+@route({routes: [
+  {
+    path: ['', 'home'],
+    component: import('./pages/home/home'),
+    title: 'Home',
+  }
+]})
+
+export class MyApp {
+
+}
