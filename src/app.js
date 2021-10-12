bc77b0ebe2eb41ff52df745a6a718b8ebd7cf5d2
Collin Herber <rustaddicts@gmail.com>
2021-10-12 18:45:43 -0400

    chore: introduce temp d2rplus files

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ import { route } from '@aurelia/router';
 @route({
   routes: [
     {
-      path: ['', 'home'],
+      path: '',
       component: import('./pages/home/home'),
       title: 'Home',
+    },
+    {
+      path: 'd2rplus',
+      component: import('./pages/d2rplus/d2rplus'),
+      title: 'D2r Plus',
     }
 ]})
 
