59189f057fe7242cf5c28654990106f653c7b37b
Collin Herber <rustaddicts@gmail.com>
2022-03-14 10:21:36 -0400

    Update Cube Search

--- src/pages/cube-recipes/cube-recipes.js
+++ src/pages/cube-recipes/cube-recipes.js
@@ -2,10 +2,33 @@ import json from '../item-jsons/cube_recipes.json';
 import {bindable} from 'aurelia';
 
 export class CubeRecipes {
-    recipes = json;
+    recipes = [...json];
     @bindable search;
 
+
     searchChanged() {
-        this.recipes = json.filter(x => x.CubeRecipeDescription.toLowerCase().includes(this.search));
+        console.log(this.search);
+        if (!this.search) {
+            this.recipes = json;
+            return;
+        }
+        let found = [];
+        for (let recipe of json) {
+            if (recipe.CubeRecipeDescription.toLowerCase().includes(this.search.toLowerCase())) {
+                found.push(recipe);
+                break;
+            }
+
+            if (recipe.Output.toLowerCase().includes(this.search.toLowerCase())) {
+                found.push(recipe);
+                break;
+            }
+
+            if (recipe.Description.toLowerCase().includes(this.search.toLowerCase())) {
+                found.push(recipe);
+                break;
+            }
+        }
+        this.recipes = found;
     }
 }
