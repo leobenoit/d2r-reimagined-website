3a9e71f4863850d6da4efac68f94d9fe511d91ba
Collin Herber <rustaddicts@gmail.com>
2024-09-25 17:37:41 -0400

    chore: add missing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import aurelia from '@aurelia/vite-plugin';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
     server: {
@@ -20,5 +21,17 @@ export default defineConfig({
     },
     plugins: [
         aurelia({ enableConventions: true, hmr: true }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'src/assets',
+                    dest: ''
+                },
+                {
+                    src: 'talonrage',
+                    dest: ''
+                }
+            ]
+        })
     ],
 });
